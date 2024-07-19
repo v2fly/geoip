36d40afed782b085c0e315562f2f2cf9f1952f38
Loyalsoldier <10487845+Loyalsoldier@users.noreply.github.com>
2024-07-20 07:22:38 +0800

    Fix: processing IPv4-mapped IPv6 address

--- lib/error.go
+++ lib/error.go
@@ -9,6 +9,8 @@ var (
 	ErrInvalidIP           = errors.New("invalid IP address")
 	ErrInvalidIPLength     = errors.New("invalid IP address length")
 	ErrInvalidIPNet        = errors.New("invalid IPNet address")
+	ErrInvalidCIDR         = errors.New("invalid CIDR")
+	ErrInvalidPrefix       = errors.New("invalid prefix")
 	ErrInvalidPrefixType   = errors.New("invalid prefix type")
 	ErrCommentLine         = errors.New("comment line")
 )
